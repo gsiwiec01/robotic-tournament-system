6385133bf08e994059a0e9f05edf68b1e6488152
KoruS <gsiwiec3131@gmail.com>
2025-05-03 00:26:50 +0200

    add UserService implementation and contract

---
+++ RTS.Modules.UserAccess.Application/Contracts/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using RTS.Modules.UserAccess.Domain.Entities;
+
+namespace RTS.Modules.UserAccess.Application.Contracts;
+
+public interface IUserService
+{
+    Task CreateAsync(User user);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+}
---
+++ RTS.Modules.UserAccess.Infrastructure/Identity/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RTS.Modules.UserAccess.Application.Contracts;
+using RTS.Modules.UserAccess.Domain.Entities;
+
+namespace RTS.Modules.UserAccess.Infrastructure.Identity;
+
+internal class UserService : IUserService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UserService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public Task CreateAsync(User user)
+    {
+        var identityUser = ApplicationUser.FromDomain(user);
+        return _userManager.CreateAsync(identityUser);
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        var identityUser = await _userManager.FindByIdAsync(id.ToString());
+        return identityUser?.ToDomain();
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        var identityUser = await _userManager.FindByEmailAsync(email);
+        return identityUser?.ToDomain();
+    }
+}
