a727b379435783c7ee3985cd481a388711778e87
KoruS <gsiwiec3131@gmail.com>
2025-05-02 22:57:09 +0200

    add mediator registration extension

---
+++ RTS.BuildingBlocks.Infrastructure/Mediation/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RTS.BuildingBlocks.Application.Mediation;
+
+namespace RTS.BuildingBlocks.Infrastructure.Mediation;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMediator(this IServiceCollection services)
+    {
+        services.AddTransient<IMediator, Mediator>();
+        
+        services.AddTransient(typeof(RequestHandlerWrapper<>));
+        services.AddTransient(typeof(RequestHandlerWrapper<,>));
+
+        return services;
+    }
+}
