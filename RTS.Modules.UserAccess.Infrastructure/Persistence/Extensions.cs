c1d01879bd0aa898e101fdec74542188cf1170db
KoruS <gsiwiec3131@gmail.com>
2025-05-02 22:58:02 +0200

    add db context for the UserAccess module

---
+++ RTS.Modules.UserAccess.Infrastructure/Persistence/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RTS.Modules.UserAccess.Infrastructure.Data;
+
+namespace RTS.Modules.UserAccess.Infrastructure.Persistence;
+
+public static class Extensions
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<UserAccessContext>(ctxOptions =>
+        {
+            ctxOptions.UseNpgsql(connectionString, opt =>
+            {
+                opt.MigrationsAssembly(typeof(UserAccessContext).Assembly.FullName);
+            });
+        });
+        
+        return services;
+    }
+}
---
+++ RTS.Modules.UserAccess.Infrastructure/Persistence/UserAccessContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RTS.Modules.UserAccess.Infrastructure.Identity;
+
+namespace RTS.Modules.UserAccess.Infrastructure.Data;
+
+internal class UserAccessContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public UserAccessContext(DbContextOptions<UserAccessContext> options) : base(options)
+    {
+    }
+}
